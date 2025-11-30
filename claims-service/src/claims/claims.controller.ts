e7cf75f5cd392a8c1f7636f0cff1e23eb3a0f90c
sujit nair <sujitnair51>
2025-11-30 18:56:48 -0500

    added pino logging and env files

--- claims-service/src/claims/claims.controller.ts
+++ claims-service/src/claims/claims.controller.ts
@@ -1,23 +1,30 @@
 import { Body, Controller, Param, Post, Get } from '@nestjs/common';
 import { ClaimsService } from './claims.service';
 import { CreateClaimDto } from './dto/create-claim.dto';
+import { Logger } from 'nestjs-pino';
 
 @Controller('api/v1/claims')
 export class ClaimsController {
-    constructor(private readonly claimsService: ClaimsService) {}
+    constructor(
+                private readonly claimsService: ClaimsService, 
+                private readonly logger: Logger
+            ) {}
 
     @Post()
     create(@Body() dto: CreateClaimDto) {
+        this.logger.log('Creating a new claim!');
         return this.claimsService.create(dto);
     }
 
     @Get(':id')
     getById(@Param('id') id: string) {
+        this.logger.log('Fetching claim for id: ');
         return this.claimsService.findById(id);
     }
 
     @Get()
     findAll() {
+        this.logger.log('Fetching all claims!');
         return this.claimsService.findAll();
     }
 
--- claims-service/src/main.ts
+++ claims-service/src/main.ts
@@ -1,15 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import './load-env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('ENV FROM NEST:', process.env.DATABASE_URL);
+  console.log('ENV FROM NEST:', process.env.NODE_ENV);
+  //  add logger - this ensures all Prisma SQL logs go through Pino
+  const logger = app.get(Logger);
+  app.useLogger(logger);
+
+  // add validation pipes
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
   }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
